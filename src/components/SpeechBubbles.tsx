fe30ed3b2c5d68c6cfd3c90b20b61e489da5c6b7
SarahBatagioti <sarah.montuanibt@gmail.com>
2023-09-14 22:23:12 -0300

    removendo useColorModeValue e personalizando

--- src/components/SpeechBubbles.tsx
+++ src/components/SpeechBubbles.tsx
@@ -6,7 +6,6 @@ import {
     Stack,
     Container,
     Avatar,
-    useColorModeValue,
   } from '@chakra-ui/react'
   
   interface Props {
@@ -24,7 +23,7 @@ import {
   
     return (
       <Stack
-        bg={useColorModeValue('white', 'gray.800')}
+        bg={'white'}
         boxShadow={'lg'}
         p={8}
         rounded={'xl'}
@@ -40,7 +39,7 @@ import {
           borderRightWidth: 16,
           borderTop: 'solid',
           borderTopWidth: 16,
-          borderTopColor: useColorModeValue('white', 'gray.800'),
+          borderTopColor: 'white',
           pos: 'absolute',
           bottom: '-16px',
           left: '50%',
@@ -67,7 +66,7 @@ import {
     return (
       <Text
         textAlign={'center'}
-        color={useColorModeValue('gray.600', 'gray.400')}
+        color={'gray.600'}
         fontSize={'sm'}>
         {children}
       </Text>
@@ -88,7 +87,7 @@ import {
         <Avatar src={src} mb={2} />
         <Stack spacing={-1} align={'center'}>
           <Text fontWeight={600}>{name}</Text>
-          <Text fontSize={'sm'} color={useColorModeValue('gray.600', 'gray.400')}>
+          <Text fontSize={'sm'} color={'white'}>
             {title}
           </Text>
         </Stack>
@@ -100,59 +99,56 @@ import {
     return (
       <Box >
         <Container maxW={'7xl'} py={16} as={Stack} spacing={12}>
-          <Stack spacing={0} align={'center'}>
-            <Heading>Our Clients Speak</Heading>
-            <Text>We have been working with clients around the world</Text>
+          <Stack spacing={0} align={'center'} textAlign={'center'}>
+            <Text className='títuloBranco'>Ouvindo o público alvo</Text>
+            <Text className='textoBranco' w={800}>Foi feita uma pesquisa de campo para consultae a opinião de pessoas com deficiencia visual e acompanhantes dos mesmos, para saber mais sobre as melhorias e utilidades na prática.</Text>
           </Stack>
           <Stack
             direction={{ base: 'column', md: 'row' }}
             spacing={{ base: 10, md: 4, lg: 10 }}>
             <Testimonial>
               <TestimonialContent>
-                <TestimonialHeading>Efficient Collaborating</TestimonialHeading>
+                <TestimonialHeading> Sair da acomodação </TestimonialHeading>
                 <TestimonialText>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor neque sed
-                  imperdiet nibh lectus feugiat nunc sem.
+                  Muitos cegos optam pela comodidade, e a bengala no desenvolvimento da criança ajudaria demais a mudar. Seria muito útil, principalmente na escola, ajudando também na questão da timidez.
                 </TestimonialText>
               </TestimonialContent>
               <TestimonialAvatar
                 src={
-                  'https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80'
+                  'https://www.teammidwest.com/webres/Image/blog/Nick.png'
                 }
-                name={'Jane Cooper'}
-                title={'CEO at ABC Corporation'}
+                name={'Alamn'}
+                title={'Indivíduo com Cegueira Tota'}
               />
             </Testimonial>
             <Testimonial>
               <TestimonialContent>
-                <TestimonialHeading>Intuitive Design</TestimonialHeading>
+                <TestimonialHeading> Atinge a todos </TestimonialHeading>
                 <TestimonialText>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor neque sed
-                  imperdiet nibh lectus feugiat nunc sem.
+                  Acredito que a bengala inteligente pode contribuir para a independência e a confiança da pessoa cega, e não só beneficiaria a pessoa com deficiência visual que a utiliza, mas também poderia impactar positivamente a vida das pessoas que convivem com o mesmo.
                 </TestimonialText>
               </TestimonialContent>
               <TestimonialAvatar
                 src={
-                  'https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80'
+                  'https://www.b-supp.co.jp/dcms_media/image/recruit04.png'
                 }
-                name={'Jane Cooper'}
-                title={'CEO at ABC Corporation'}
+                name={'Rutilene Sousa'}
+                title={'Gerente da Educação'}
               />
             </Testimonial>
             <Testimonial>
               <TestimonialContent>
-                <TestimonialHeading>Mindblowing Service</TestimonialHeading>
+                <TestimonialHeading> Ideia factível </TestimonialHeading>
                 <TestimonialText>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor neque sed
-                  imperdiet nibh lectus feugiat nunc sem.
+                  Bem viável, principalmente para crianças dentro de casa, trará autonomia sob diversos aspectos. Será muito satisfatório para o nosso “mundo cegal” 
                 </TestimonialText>
               </TestimonialContent>
               <TestimonialAvatar
                 src={
-                  'https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80'
+                  'https://www.oozora-sakuranoki.com/wp-content/uploads/2023/07/%E5%90%8D%E7%A7%B0%E6%9C%AA%E8%A8%AD%E5%AE%9A%E3%81%AE%E3%83%87%E3%82%B6%E3%82%A4%E3%83%B3-2023-07-06T125821.358-768x768.png'
                 }
-                name={'Jane Cooper'}
-                title={'CEO at ABC Corporation'}
+                name={'Diego'}
+                title={'Indivíduo com Cegueira Tota'}
               />
             </Testimonial>
           </Stack>
